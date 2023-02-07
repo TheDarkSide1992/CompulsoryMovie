3b85a1f5210f01ab9f832e4ba366e463e6cb13a5
TheDarkSide1992 <Jenscp171@gmail.com>
2023-02-07 14:06:14 +0100

    Log-In system Update

--- MovieRecommendationSystem-CachedImplementation-main/src/dk/easv/presentation/Controller/IndexController.java
+++ MovieRecommendationSystem-CachedImplementation-main/src/dk/easv/presentation/Controller/IndexController.java
@@ -5,12 +5,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.security.interfaces.ECKey;
 import java.util.ResourceBundle;
 
 public class IndexController implements Initializable {
@@ -18,15 +23,37 @@ public class IndexController implements Initializable {
     @FXML private Button btnMenu;
     private LogicManager logicManager;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         logicManager = new LogicManager();
 
         try {
-            loadCenter("LogIn.fxml");
+            loadLogIn();
             loadTop("Menu.fxml");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void loadLogIn() throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/dk/easv/presentation/View/LogIn.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        LogInController logInController = fxmlLoader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Log-In");
+        stage.setScene(new Scene(root1));
+        stage.showAndWait();
+
+
+
+
+        //TODO Gets data form login controller
+
+        if(logInController.isLoginSuccessful()){
+            loadTop("Menu.fxml");
+            //TODO Get USer Data
         }
 
     }
--- MovieRecommendationSystem-CachedImplementation-main/src/dk/easv/presentation/Controller/LogInController.java
+++ MovieRecommendationSystem-CachedImplementation-main/src/dk/easv/presentation/Controller/LogInController.java
@@ -3,16 +3,12 @@ package dk.easv.presentation.Controller;
 import dk.easv.logic.LogicManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -24,43 +20,40 @@ public class LogInController implements Initializable {
     @FXML private TextField userId;
 
     private LogicManager logicManager;
+    private boolean loginSauces = false;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        logicManager = new LogicManager();
         btnLogIn.setDisable(true);
         btnSignUp.setDisable(false);
     }
 
     public void logIn(ActionEvent actionEvent) {
-        logicManager = new LogicManager();
-        System.out.println("Username is: " + userId.getText() + "\nPassword is: " + passwordField.getText());
-
+        loginSauces = true;
         Stage stage = (Stage) btnLogIn.getScene().getWindow();
         stage.close();
     }
 
     public void signUp(ActionEvent actionEvent) {
-       logicManager = new LogicManager();
-       System.out.println("Username is: " + userId.getText() + "\nPassword is: " + passwordField.getText());
-       logicManager.openNewView("Index.fxml", "Movie recommendation system");
+
     }
 
     public void checkIfAnyInput(KeyEvent keyEvent) {
         if(!userId.getText().equals("")){
             if(!passwordField.getText().equals("")){
-                btnSignUp.setDisable(false);
                 btnLogIn.setDisable(false);
             }
             else {
-                btnSignUp.setDisable(true);
                 btnLogIn.setDisable(true);
             }
         }
         else {
-            btnSignUp.setDisable(true);
             btnLogIn.setDisable(true);
         }
     }
+
+    public boolean isLoginSuccessful() {
+        return loginSauces;
+    }
+
 }
