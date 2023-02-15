287044057c239d1afa78134371aff2fd6ad37c22
TheDarkSide1992 <Jenscp171@gmail.com>
2023-02-15 10:25:08 +0100

    close movie info window

--- src/dk/easv/GUI/Controller/MovieInfoController.java
+++ src/dk/easv/GUI/Controller/MovieInfoController.java
@@ -1,17 +1,25 @@
 package dk.easv.GUI.Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MovieInfoController implements Initializable {
-    public void HandleClose(MouseEvent mouseEvent) {
-    }
+
+    @FXML private Circle circleClose;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void HandleClose(MouseEvent mouseEvent) {
+        Stage stage = (Stage) circleClose.getScene().getWindow();
+        stage.close();
     }
 }
