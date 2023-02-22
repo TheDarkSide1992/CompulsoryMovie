3edb0bb26c3c542239a4ab5e2523a44fb3a8366b
hjorthen1243 <114651351+hjorthen1243@users.noreply.github.com>
2023-02-22 09:30:42 +0100

    Movie info virker

--- src/dk/easv/GUI/Controller/MovieInfoController.java
+++ src/dk/easv/GUI/Controller/MovieInfoController.java
@@ -41,32 +41,50 @@ public class MovieInfoController implements Initializable {
 
     public void setInfoForMovie(String movieTitle, int year) {
         title = movieTitle;
-        txtTitle.setText(movieTitle + "(" + year + ")");
+        txtTitle.setText(movieTitle);
+        TextYear.setText(String.valueOf(year));
         String movieInfoString = movieInfoModel.getMovieInfo(title, year);
 
+        if(movieInfoString.contains("Response\":\"False\",\"Error\":\"Movie not found!")){
+            String director = "N/A";
+            String writer = "N/A";
+            String actor = "N/A";
+            String genre = "N/A";
+            String runtime = "N/A";
+            String ageRestriction = "N/A";
+            String description = "N/A";
+            String RTScore = "N/A";
+            String ImdbScore = "N/A";
 
-        String director = movieInfoString.substring(movieInfoString.indexOf("Director") + 11, movieInfoString.indexOf("Writer") - 3);
-        String writer = movieInfoString.substring(movieInfoString.indexOf("Writer") + 9, movieInfoString.indexOf("Actors") - 3);
-        String actor = movieInfoString.substring(movieInfoString.indexOf("Actors") + 9, movieInfoString.indexOf("Plot") - 3);
-        String genre = movieInfoString.substring(movieInfoString.indexOf("Genre") + 8, movieInfoString.indexOf("Director") - 3);
-        String runtime = movieInfoString.substring(movieInfoString.indexOf("Runtime") + 10, movieInfoString.indexOf("Genre") - 3);
-        String ageRestriction = movieInfoString.substring(movieInfoString.indexOf("Rated") + 8, movieInfoString.indexOf("Released") - 3);
-
-        txtInfo.setText("Director: " + director + "\n" + "Writers: " + writer + "\n" + "Actors: " + actor + "\n" + "Genre: " + genre
-                + "\n" + "Runtime: " + runtime + "\n" + "Age restriction: " + ageRestriction);
-        String description = movieInfoString.substring(movieInfoString.indexOf("Plot") + 7, movieInfoString.indexOf("Language") - 3);
-        txtDescription.setText(description);
-        String RTScore = null;
-        if (movieInfoString.contains("Rotten Tomatoes")) {
-            RTScore = movieInfoString.substring(movieInfoString.indexOf("Rotten Tomatoes")+26, movieInfoString.indexOf("%")+1);
+            txtRTomato.setText(RTScore);
+            txtDescription.setText(description);
+            txtIMDB.setText(ImdbScore);
+            txtInfo.setText("Director: " + director + "\n" + "Writers: " + writer + "\n" + "Actors: " + actor + "\n" + "Genre: " + genre
+                    + "\n" + "Runtime: " + runtime + "\n" + "Age restriction: " + ageRestriction);
         }
         else {
-            RTScore = "N/A";
-        }
-        txtRTomato.setText(RTScore);
-        String ImdbScore = movieInfoString.substring(movieInfoString.indexOf("imdbRating") + 13, movieInfoString.indexOf("imdbVotes") - 3);
-        txtIMDB.setText(ImdbScore);
-    }
+            String director = movieInfoString.substring(movieInfoString.indexOf("Director") + 11, movieInfoString.indexOf("Writer") - 3);
+            String writer = movieInfoString.substring(movieInfoString.indexOf("Writer") + 9, movieInfoString.indexOf("Actors") - 3);
+            String actor = movieInfoString.substring(movieInfoString.indexOf("Actors") + 9, movieInfoString.indexOf("Plot") - 3);
+            String genre = movieInfoString.substring(movieInfoString.indexOf("Genre") + 8, movieInfoString.indexOf("Director") - 3);
+            String runtime = movieInfoString.substring(movieInfoString.indexOf("Runtime") + 10, movieInfoString.indexOf("Genre") - 3);
+            String ageRestriction = movieInfoString.substring(movieInfoString.indexOf("Rated") + 8, movieInfoString.indexOf("Released") - 3);
+            String description = movieInfoString.substring(movieInfoString.indexOf("Plot") + 7, movieInfoString.indexOf("Language") - 3);
+
+            String RTScore = null;
+            if (movieInfoString.contains("Rotten Tomatoes")) {
+                RTScore = movieInfoString.substring(movieInfoString.indexOf("Rotten Tomatoes") + 26, movieInfoString.indexOf("%") + 1);
+            } else {
+                RTScore = "N/A";
+            }
+            String ImdbScore = movieInfoString.substring(movieInfoString.indexOf("imdbRating") + 13, movieInfoString.indexOf("imdbVotes") - 3);
+
+            txtRTomato.setText(RTScore);
+            txtDescription.setText(description);
+            txtIMDB.setText(ImdbScore);
+            txtInfo.setText("Director: " + director + "\n" + "Writers: " + writer + "\n" + "Actors: " + actor + "\n" + "Genre: " + genre
+                    + "\n" + "Runtime: " + runtime + "\n" + "Age restriction: " + ageRestriction);
+    }}
 
 
     public void HandleClose(MouseEvent mouseEvent) {
